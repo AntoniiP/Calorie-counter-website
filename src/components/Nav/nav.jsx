9d3a8d6266451079420caf2d39d522dcacbce49b
antonii1357 <antonii.spam@gmail.com>
2024-01-21 12:19:26 -0600

    Fixed nav css and added icons.

--- src/components/Nav/nav.jsx
+++ src/components/Nav/nav.jsx
@@ -11,34 +11,34 @@ export default function Nav() {
 				</li>
 				<li className='nav-item'>
 					<a href='#' className='nav-link'>
-						<i className='nav-icon search-icon'></i>
+						<i className='nav-icon meals-icon'></i>
 					</a>
 				</li>
 				<li className='nav-item'>
 					<a href='#' className='nav-link'>
-						<i className='nav-icon search-icon'></i>
+						<i className='nav-icon notes-icon'></i>
 					</a>
 				</li>
 				<li className='nav-item'>
 					<a href='#' className='nav-link'>
-						<i className='nav-icon search-icon'></i>
+						<i className='nav-icon time-icon'></i>
 					</a>
 				</li>
 				<div className='nav-separator'></div>
 				<li className='nav-item'>
 					<a href='#' className='nav-link'>
-						<i className='nav-icon search-icon'></i>
+						<i className='nav-icon settings-icon'></i>
 					</a>
 				</li>
 				<li className='nav-item'>
 					<a href='#' className='nav-link'>
-						<i className='nav-icon search-icon'></i>
+						<i className='nav-icon help-icon'></i>
 					</a>
 				</li>
 				
 				<li className='nav-item'>
 					<a href='#' className='nav-link'>
-						<i className='nav-icon search-icon'></i>
+						<i className='nav-icon logout-icon'></i>
 					</a>
 				</li>
 			</ul>
