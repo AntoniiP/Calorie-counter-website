86ad92dabf3537dbe2e09f3eae321d29e6b5fee7
antonii1357 <antonii.spam@gmail.com>
2024-01-24 18:39:33 -0600

    Added the start of the register route. Needs testing & finishing

---
+++ backend/functions.js
@@ -0,0 +1,10 @@
+function HashPassword(password) {
+    // Procedure to hash TODO:
+    // Generate a salt
+    // Hash password with salt
+    
+    return password
+}
+
+
+module.exports = { HashPassword }
--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,27 @@
 const express = require('express'),
 	mongoose = require('mongoose'),
 	app = express(),
-	PORT = 8706
+    PORT = 8706,
+    db = require('./models/user'),
+    { HashPassword } = require('./functions')
 
 mongoose.connect('mongodb://127.0.0.1:27017/calcounter').then(() => console.log('Connected to DB.'), console.log('Error connecting to DB.'))
 
-app.get('/register', (req, res) => {})
+app.get('/register', async (req, res) => {
+	const {username, calorieCount, proteinCount, password} = req.body
+	if (!username || !calorieCount || !proteinCount || !password) return res.status(400)
+	if (await db.findOne({username})) return res.send(409) // "The request could not be completed due to a conflict with the current state of the resource. This code is only allowed in situations where it is expected that the user might be able to resolve the conflict and resubmit the request. " - Stackoverflow 3825990
+
+    await db.create({
+		username, // Character limit needed in the future
+		password: HashPassword(password),
+		totalCalories: Number,
+		totalProtein: Number,
+		currentCalories: Number,
+		currentProtein: Number
+	})
+
+})
 
 app.get('/login', (req, res) => {})
 
