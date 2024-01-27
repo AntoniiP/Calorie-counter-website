b42a1a60263dc90a1a5d510fa8658dfa98fb31c9
antonii1357 <antonii.spam@gmail.com>
2024-01-27 13:58:05 -0600

    Added login route

--- backend/index.js
+++ backend/index.js
@@ -5,11 +5,16 @@ const express = require('express'),
 	app = express(),
     PORT = 8706,
     db = require('./models/user'),
-    { HashPassword } = require('./functions')
+	{ HashPassword } = require('./functions'),
+	bodyparser = require('body-parser'),
+	cors = require('cors')
 
-mongoose.connect(process.env.db).then(() => console.log('Connected to DB.'), console.log('Error connecting to DB.'))
 
-app.get('/register', async (req, res) => {
+app.use(bodyparser.json(), bodyparser.urlencoded({ extended: true }), cors())
+
+mongoose.connect(process.env.db).then(() => console.log('Connected to DB.'), (er) => console.log('Error connecting to DB.', er))
+
+app.post('/register', async (req, res) => {
 	const {username, calorieCount, proteinCount, password} = req.body
 	if (!username || !calorieCount || !proteinCount || !password) return res.status(400)
 	if (await db.findOne({username})) return res.send(409) // "The request could not be completed due to a conflict with the current state of the resource. This code is only allowed in situations where it is expected that the user might be able to resolve the conflict and resubmit the request. " - Stackoverflow 3825990
@@ -25,7 +30,18 @@ app.get('/register', async (req, res) => {
 
 })
 
-app.get('/login', (req, res) => {})
+
+app.post('/login', async (req, res) => {
+	const { username, password } = req.body
+	if (!username || !password) return res.status(400)
+	console.log(req.body, {username, password: HashPassword(password)})
+	const user = await db.findOne({ username, password: HashPassword(password) })
+	if (!user) return res.status(404).send('No user found.')
+	const {totalCalories, totalProtein, currentCalories, currentProtein, mealLog, meals, food} = user
+	return res.send({ totalCalories, totalProtein, currentCalories, currentProtein, mealLog, meals, food })
+
+
+})
 
 app.get('/meallog', (req, res) => {})
 
