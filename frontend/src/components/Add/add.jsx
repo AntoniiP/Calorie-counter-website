695bd1975ebee61eef0b315fa779d885bc71d26f
antonii1357 <antonii.spam@gmail.com>
2024-02-18 10:37:57 -0600

    Added cross svg for closing intake div

--- frontend/src/components/Add/add.jsx
+++ frontend/src/components/Add/add.jsx
@@ -27,31 +27,38 @@ export default function Add({ toggleDiv }) {
 	}
 
 	return (
-		<div className="add-wrapper">
-
-		<div className='add-cal'>
-			<h4>Add your custom intake or select an item from a specific brand.</h4>
-
-			<div className='button-cover'>
-				<div className='button r' id='button-1'>
-					<input type='checkbox' className='checkbox' onClick={toggleSwitch} />
-					<div className='knobs'></div>
-					<div className='layer'></div>
+		<div className='add-wrapper'>
+			<div className='add-cal'>
+				<h4>Add your custom intake or select an item from a specific brand.</h4>
+				<div className='close-div' onClick={toggleDiv}>
+					<svg width='40' height='40' viewBox='0 0 100 100' xmlns='http://www.w3.org/2000/svg'>
+						<rect x='40' y='10' width='20' height='80' rx='10' ry='10' />
+						<rect x='10' y='40' width='80' height='20' rx='10' ry='10' />
+					</svg>
 				</div>
+
+				<div className='button-cover'>
+					<div className='button r' id='button-1'>
+						<input type='checkbox' className='checkbox' onClick={toggleSwitch} />
+						<div className='knobs'></div>
+						<div className='layer'></div>
+					</div>
+				</div>
+
+				{isActive ? (
+					<div className='custom-intake'>
+						<p>Calories (cal)</p>
+						<input type='number' min='10' name='Calories' placeholder='Daily calories goal (cal)' id='cal' onKeyDown={handleKeyDown} />
+						<p>Protein (g)</p>
+						<input type='number' min='1' name='Protein' placeholder='Daily protein goal (g)' id='prote' onKeyDown={handleKeyDown} />
+						<button className='button' onClick={addCount}>
+							Add to the count
+						</button>
+					</div>
+				) : (
+					<div className='brands'>{/* Add brands */}</div>
+				)}
 			</div>
-
-			{isActive ? (
-				<div className='custom-intake'>
-					<p>Calories (cal)</p>
-					<input type='number' min='10' name='Calories' placeholder='Daily calories goal (cal)' id='cal' onKeyDown={handleKeyDown} />
-					<p>Protein (g)</p>
-					<input type='number' min='1' name='Protein' placeholder='Daily protein goal (g)' id='prote' onKeyDown={handleKeyDown}/>
-					<button className="button" onClick={addCount}>Add to the count</button>
-				</div>
-			) : (
-				<div className='brands'>{/* Add brands */}</div>
-			)}
-		</div>
 		</div>
 	)
 }
