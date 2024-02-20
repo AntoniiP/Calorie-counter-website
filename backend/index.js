da44984dc5443a47bc262d8b363aba97bbac9c5b
antonii1357 <antonii.spam@gmail.com>
2024-02-19 18:12:15 -0600

    Added get brands route to fetch all brands existing in the server with their logo.

--- backend/index.js
+++ backend/index.js
@@ -3,8 +3,13 @@ require('dotenv').config()
 const express = require('express'),
 	mongoose = require('mongoose'),
 	bodyparser = require('body-parser'),
-	bcrypt = require('bcrypt')
-;(app = express()), (PORT = 8706), (db = require('./models/user')), ({generateSalt, hashPassword} = require('./functions')), (cors = require('cors'))
+	bcrypt = require('bcrypt'),
+	app = express(),
+	PORT = 8706,
+	db = require('./models/user'),
+	{hashPassword} = require('./functions'),
+	cors = require('cors'),
+	fs = require('fs')
 
 app.use(bodyparser.json(), bodyparser.urlencoded({extended: true}), cors())
 
@@ -15,15 +20,15 @@ mongoose.connect(process.env.db).then(
 
 app.post('/register', async (req, res) => {
 	const {username, totalCalories, totalProtein, password} = req.body
-	if (!username || !totalCalories || !totalProtein || !password) return res.status(400).send({ error: 'All fields are required.'})
-	if (username.length < 5 || username.length > 20) return res.status(400).send({ error: 'Username must be between 5 and 20 characters.' })
-	if (password.length < 5) return res.status(400).send({ error: 'Password must be between minimum 5 characters.' })
-	if (await db.findOne({username})) return res.status(409).send({ error: 'Account already exists.'}) // "The request could not be completed due to a conflict with the current state of the resource. This code is only allowed in situations where it is expected that the user might be able to resolve the conflict and resubmit the request. " - Stackoverflow 3825990
-	
+	if (!username || !totalCalories || !totalProtein || !password) return res.status(400).send({error: 'All fields are required.'})
+	if (username.length < 5 || username.length > 20) return res.status(400).send({error: 'Username must be between 5 and 20 characters.'})
+	if (password.length < 5) return res.status(400).send({error: 'Password must be between minimum 5 characters.'})
+	if (await db.findOne({username})) return res.status(409).send({error: 'Account already exists.'}) // "The request could not be completed due to a conflict with the current state of the resource. This code is only allowed in situations where it is expected that the user might be able to resolve the conflict and resubmit the request. " - Stackoverflow 3825990
+
 	const hash = await hashPassword(password)
 
 	await db.create({
-		username, 
+		username,
 		password: hash,
 		totalCalories,
 		totalProtein,
@@ -31,19 +36,18 @@ app.post('/register', async (req, res) => {
 		currentProtein: 0
 	})
 
-	return res.status(200).send({ totalCalories, totalProtein })
+	return res.status(200).send({totalCalories, totalProtein})
 })
 
 app.post('/login', async (req, res) => {
 	const {username, password} = req.body
-	if (!username || !password) return res.status(400).send({ error: 'Username and password are required.' })
-	
+	if (!username || !password) return res.status(400).send({error: 'Username and password are required.'})
+
 	try {
 		const user = await db.findOne({username})
 		if (!user) return res.status(404).send({error: 'No user found.'})
-		
+
 		const isMatch = await bcrypt.compare(password, user.password)
-		
 
 		if (!isMatch) return res.status(401).send({error: 'Invalid credentials.'})
 
@@ -53,6 +57,26 @@ app.post('/login', async (req, res) => {
 		console.error(er)
 		return res.status(500).send({error: 'An error occurred while processing your request.'})
 	}
+})
+
+app.get('/brands', (req, res) => {
+	const files = fs.readdirSync('./brandsData')
+	
+	const names = files.map((a) => a.split('.')[0])
+	const icons  = files.map(x => require('./brandsData/' + x)).map(x => x.icon)
+	
+	const brandIconsObject = names.reduce((obj, brand, index) => {
+		obj[brand] = {icon: icons[index]}
+		return obj
+	}, {}) 
+	// Returns an object in the form of
+	// 	{
+	//		"Name": {
+	// 			icon: "iconURL"
+	// 		}
+	// 	}
+
+	res.send(brandIconsObject)
 })
 
 app.get('/meallog', (req, res) => {})
