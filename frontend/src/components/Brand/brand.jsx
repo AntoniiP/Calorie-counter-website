57cee23d1c1d960506d0c78903c8daf57caa5dab
AntoniiP <75284544+AntoniiP@users.noreply.github.com>
2024-02-26 12:48:50 -0600

    Added small brand component

--- frontend/src/components/Brand/brand.jsx
+++ frontend/src/components/Brand/brand.jsx
@@ -4,5 +4,22 @@ import {useState, useContext} from 'react'
 import useFetch from '../../hooks/useFetch'
 
 export default function Brand({name, icon}) {
-	return <img className={'brand-icon ' + name} src={icon} alt=''></img>
+	const [isRequestMade, setIsRequestMade] = useState(false)
+	const [isActive, setActive] = useState(true)
+	const {updateCount} = useContext(AppContext)
+	const {postData, getData} = useFetch()
+
+	async function getBrandData(name) {
+		if (!isRequestMade) {
+			const res = await getData('http://localhost:8706/brands/' + name)
+			if (!res.error) {
+				setBrandsData(res)
+				setIsRequestMade(true)
+			}
+		}
+	}
+
+	return (
+		<img className={'brand-icon'} src={icon} alt='' onClick={() => getBrandData(name)}></img>
+	)
 }
