de3d6d12a46e53e66d83a9e9c66b5a39992ec7ad
antonii1357 <antonii.spam@gmail.com>
2024-05-04 18:50:09 -0600

    Improved card component, needs probably a rewrite for future usages Needs more personalization, border, size, box-shadow, colors, etc.

--- frontend/src/components/Card/card.jsx
+++ frontend/src/components/Card/card.jsx
@@ -1,19 +1,23 @@
 import './card.css'
-export default function Card({ name, content, sortType, button }) {
+export default function Card({name, title, content, sortType, button, bottomButton, size, border, shadow}) {
 	return (
 		<div className='card main-card'>
-			<div className="text">
-				<h3>{ name }</h3>
-			</div>
-			<div className="card-c">
-				{button && <button className='content button'>+</button>}
-				<div className="content"></div>
-				<div className="content"></div>
-				<div className="content"></div>
-				<div className="content"></div>
-				<div className="content"></div>
-				<div className="content"></div>
-				<div className="content"></div>
+			<div className='text'>{title ? <h2>{title}</h2> : <h3>{name}</h3>}</div>
+			{content && <p>{content}</p>}
+			<div className='card-c'>
+				{button && !bottomButton && <button className='content button'>{typeof button == 'string' ? 'See recipe' : '+'}</button>}
+				{!content && (
+					<div className='contentDivs'>
+						<div className='content'></div>
+						<div className='content'></div>
+						<div className='content'></div>
+						<div className='content'></div>
+						<div className='content'></div>
+						<div className='content'></div>
+						<div className='content'></div>
+					</div>
+				)}{' '}
+				{button && bottomButton && <button className='content button textButton'>{typeof button == 'string' ? 'See recipe' : '+'}</button>}
 			</div>
 		</div>
 	)
--- frontend/src/components/recipes/recipes.jsx
+++ frontend/src/components/recipes/recipes.jsx
@@ -9,11 +9,9 @@ export default function Recipes() {
 				Suggested recipes based on <span className='highlight'>your food.</span>
 			</h2>
 			<h3>Breakfast and quick meals</h3>
-			{/* <div className='cards'> */}
-				{/* <Card name='Meal log' /> */}
-				{/* <Card name='My meals' button /> */}
-				{/* <Card name='My food' button /> */}
-			{/* </div> */}
+			<div className='small-cards'>
+				<Card title='Chicken sandwich' content={"Calories: 250 cal\nProtein: 20g"} button="See recipe" bottomButton />
+			</div>
 			<h3>Lunch</h3>
 			<h3>Dinner</h3>
 		</div>
