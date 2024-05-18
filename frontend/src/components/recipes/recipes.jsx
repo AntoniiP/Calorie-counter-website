2187d2aa77e4c60ea5299f9d4a9fcd8ff11e9649
antonii1357 <antonii.spam@gmail.com>
2024-05-18 11:29:02 -0600

    Added get request for recipes, and updated css

--- frontend/src/components/recipes/recipes.jsx
+++ frontend/src/components/recipes/recipes.jsx
@@ -1,8 +1,20 @@
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 import Card from '../Card/card'
+import useFetch from '../../hooks/useFetch'
 import './recipes.css'
 
 export default function Recipes() {
+	const { postData, getData } = useFetch()
+	const [data, setData] = useState([])
+
+	async function getRecipes() {
+		const res = await getData('http://localhost:8706/recipes')
+		setData(res)
+	}
+
+	useEffect(() => {
+		getRecipes()
+	}, [])
     return (
 		<div className='recipes'>
 			<h2>
@@ -10,10 +22,16 @@ export default function Recipes() {
 			</h2>
 			<h3>Breakfast and quick meals</h3>
 			<div className='small-cards'>
-				<Card title='Chicken sandwich' content={"Calories: 250 cal\nProtein: 20g"} button="See recipe" bottomButton />
+				{data.filter(x => x.type == 'breakfast').map((x, i) => <Card key={i} title={x.name} content={"Calories: " + x.calories + ' cal. Protein: ' + x.protein + 'g'} button={'See recipe'} bottomButton />)}
 			</div>
 			<h3>Lunch</h3>
+			<div className='small-cards'>
+				{data.filter(x => x.type == 'lunch').map((x, i) => <Card key={i} title={x.name} content={"Calories: " + x.calories + ' cal. Protein: ' + x.protein + 'g'} button={'See recipe'} bottomButton />)}
+			</div>
 			<h3>Dinner</h3>
+			<div className='small-cards'>
+				{data.filter(x => x.type == 'dinner').map((x, i) => <Card key={i} title={x.name} content={"Calories: " + x.calories + ' cal. Protein: ' + x.protein + 'g'} button={'See recipe'} bottomButton />)}
+			</div>
 		</div>
 	)
 }
